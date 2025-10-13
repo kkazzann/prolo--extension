7acdac81f4c3efe37e42f078dc5afccb60001a84
kkazzann <contact@kznlabs.com>
2025-10-13 16:59:43 +0200

    chore: change boilerplate naming to prolo extension rewritten

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,6 +4,10 @@ import { defineConfig } from 'wxt';
 export default defineConfig({
   srcDir: 'src',
   modules: ['@wxt-dev/module-react'],
+  manifest: {
+    permissions: ['tabs', 'activeTab'],
+    host_permissions: ['*://*.prologistics.info/*']
+  },
   webExt: {
     openDevtools: true,
     startUrls: ['https://prologistics.info', 'https://prolodev.prologistics.info'],
